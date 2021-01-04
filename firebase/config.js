611ead74ca260e01b7327154ac0e59d2fa67b4ad
bella <bella.royle@gmail.com>
2021-01-04 11:44:34 +0000

    set up repo with react-native and added firebase config

---
+++ firebase/config.js
@@ -0,0 +1,19 @@
+import * as firebase from "firebase";
+import "@firebase/auth";
+import "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8jQlIJ1rYySPq9PVwsoFapoXRtpyPeag",
+  //   authDomain: 'your-auth-domain-b1234.firebaseapp.com',
+  databaseURL: "https://caption-game-9081a.firebaseio.com",
+  projectId: "caption-game-9081a",
+  storageBucket: "caption-game-9081a.appspot.com",
+  //   messagingSenderId: "12345-insert-yourse",
+  appId: "1:529864157432:ios:91d182e6b213465e17330d",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
