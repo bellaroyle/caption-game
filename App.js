829022c3b719cf860f4e09855526351b58d33831
bella <bella.royle@gmail.com>
2021-01-04 17:19:40 +0000

    fixed error in get users in room

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, Button, View } from 'react-native';
-import { createRoom } from './src/utils/databaseFuncs';
+import React from "react";
+import { StyleSheet, Text, Button, View } from "react-native";
+import {
+  createRoom,
+  joinRoom,
+  getUsersInRoom,
+} from "./src/utils/databaseFuncs";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>There is a button here</Text>
-      <Button onPress={() => createRoom('Tom')} title="Create room" />
+      <Button onPress={() => createRoom("Tom")} title="Create room" />
+      <Button onPress={() => joinRoom("AhSZNY", "Tom")} title="join room" />
+      <Button onPress={() => getUsersInRoom("AhSZNY")} title="get users" />
     </View>
   );
 }
@@ -15,7 +21,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     // backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
--- src/utils/databaseFuncs.js
+++ src/utils/databaseFuncs.js
@@ -1,15 +1,33 @@
-import { firebase } from '../firebase/config';
-import { randomCodeGen } from './utils';
+import { firebase } from "../firebase/config";
+import { randomCodeGen } from "./utils";
 
-const rooms = firebase.firestore().collection('rooms');
+const rooms = firebase.firestore().collection("rooms");
 
 const createRoom = (username) => {
   const roomCode = randomCodeGen();
   return rooms
     .doc(roomCode)
-    .collection('users')
+    .collection("users")
     .doc()
     .set({ host: true, name: username, points: 0 });
 };
 
-module.exports = { rooms, createRoom };
+const joinRoom = (roomCode, username) => {
+  return rooms
+    .doc(roomCode)
+    .collection("users")
+    .doc()
+    .set({ host: false, name: username, points: 0 });
+};
+
+const getUsersInRoom = (roomCode) => {
+  return rooms
+    .doc(roomCode)
+    .collection("users")
+    .get()
+    .then((snapshot) => {
+      snapshot.docs.map((doc) => console.log(doc.data()));
+    });
+};
+
+module.exports = { rooms, createRoom, joinRoom, getUsersInRoom };
