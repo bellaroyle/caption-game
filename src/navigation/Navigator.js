b02ba654c3deb1226fafcb21280ea34341bad144
THPHogg <t.hogg@hotmail.com>
2021-01-05 10:46:05 +0000

    started waiting room and navigation to it

--- src/navigation/Navigator.js
+++ src/navigation/Navigator.js
@@ -1,10 +1,11 @@
-import React from "react";
-import {} from "react-native";
-import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
-import Welcome from "../screens/Welcome/Welcome";
-import Host from "../screens/Host/Host";
-import Join from "../screens/Join/Join";
+import React from 'react';
+import {} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Welcome from '../screens/Welcome/Welcome';
+import Host from '../screens/Host/Host';
+import Join from '../screens/Join/Join';
+import WaitingRoom from '../screens/WaitingRoom/WaitingRoom';
 
 export default function Navigator() {
   return (
@@ -22,6 +23,7 @@ const GameStackNavigator = () => {
       <GameStack.Screen name="Welcome" component={Welcome} />
       <GameStack.Screen name="Host" component={Host} />
       <GameStack.Screen name="Join" component={Join} />
+      <GameStack.Screen name="WaitingRoom" component={WaitingRoom} />
     </GameStack.Navigator>
   );
 };
---
+++ src/screens/WaitingRoom/WaitingRoom.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function WaitingRoom() {
+  return (
+    <View>
+      <Text>Waiting Room</Text>
+    </View>
+  );
+}
