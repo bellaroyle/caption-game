1d7d7943e5ebe22d46fa089a8c7ab0b7b253ab99
THPHogg <t.hogg@hotmail.com>
2021-01-05 10:08:57 +0000

    completed getUsersInRoom and added functionality to make sure there is only one person in the room with a given name

--- src/utils/databaseFuncs.js
+++ src/utils/databaseFuncs.js
@@ -1,33 +1,44 @@
-import { firebase } from "../firebase/config";
-import { randomCodeGen } from "./utils";
+import { firebase } from '../firebase/config';
+import { randomCodeGen } from './utils';
+import { Alert } from 'react-native';
 
-const rooms = firebase.firestore().collection("rooms");
+const rooms = firebase.firestore().collection('rooms');
 
 const createRoom = (username) => {
   const roomCode = randomCodeGen();
   return rooms
     .doc(roomCode)
-    .collection("users")
+    .collection('users')
     .doc()
     .set({ host: true, name: username, points: 0 });
-};
-
-const joinRoom = (roomCode, username) => {
-  return rooms
-    .doc(roomCode)
-    .collection("users")
-    .doc()
-    .set({ host: false, name: username, points: 0 });
 };
 
 const getUsersInRoom = (roomCode) => {
   return rooms
     .doc(roomCode)
-    .collection("users")
+    .collection('users')
     .get()
     .then((snapshot) => {
-      snapshot.docs.map((doc) => console.log(doc.data()));
+      return snapshot.docs.map((doc) => {
+        console.log(doc.data().name);
+        return doc.data().name;
+      });
     });
+};
+
+const joinRoom = (roomCode, username) => {
+  return getUsersInRoom(roomCode).then((users) => {
+    return users.includes(username)
+      ? Alert.alert('Username in use', 'Please change your name!', {
+          text: 'OK',
+          onPress: () => console.log('OK Pressed'),
+        })
+      : rooms
+          .doc(roomCode)
+          .collection('users')
+          .doc()
+          .set({ host: false, name: username, points: 0 });
+  });
 };
 
 module.exports = { rooms, createRoom, joinRoom, getUsersInRoom };
