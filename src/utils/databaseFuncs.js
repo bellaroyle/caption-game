f1c798669b9da783d3b0f64af4c30cbd9418344c
THPHogg <t.hogg@hotmail.com>
2021-01-06 12:04:51 +0000

    able to access firebase storage and display image when first round starts

--- src/utils/databaseFuncs.js
+++ src/utils/databaseFuncs.js
@@ -1,8 +1,8 @@
-import { firebase } from "../firebase/config";
-import { randomCodeGen } from "./utils";
-import { Alert } from "react-native";
+import { firebase } from '../firebase/config';
+import { randomCodeGen } from './utils';
+import { Alert } from 'react-native';
 
-const rooms = firebase.firestore().collection("rooms");
+const rooms = firebase.firestore().collection('rooms');
 
 const createRoom = (username) => {
   const roomCode = randomCodeGen();
@@ -12,27 +12,19 @@ const createRoom = (username) => {
     .then(() => {
       return rooms
         .doc(roomCode)
-        .collection("users")
+        .collection('users')
         .doc()
         .set({ host: true, name: username, points: 0 })
         .then(() => {
           return roomCode;
         });
     });
-  // return rooms
-  //   .doc(roomCode)
-  //   .collection("users")
-  //   .doc()
-  //   .set({ host: true, name: username, points: 0 })
-  //   .then(() => {
-  //     return roomCode;
-  //   });
 };
 
 const getUsersInRoom = (roomCode) => {
   return rooms
     .doc(roomCode)
-    .collection("users")
+    .collection('users')
     .get()
     .then((snapshot) => {
       return snapshot.docs.map((doc) => {
@@ -43,7 +35,7 @@ const getUsersInRoom = (roomCode) => {
 };
 
 const doesRoomExist = (roomCode) => {
-  console.log(roomCode, "room code ");
+  console.log(roomCode, 'room code ');
   return rooms
     .doc(roomCode)
     .get()
@@ -55,27 +47,32 @@ const doesRoomExist = (roomCode) => {
 };
 
 const joinRoom = (roomCode, username) => {
-  console.log(roomCode, "room code in join room");
+  console.log(roomCode, 'room code in join room');
   return doesRoomExist(roomCode).then((roomExists) => {
-    console.log("does room exist", roomExists);
+    console.log('does room exist', roomExists);
     return roomExists
       ? getUsersInRoom(roomCode).then((users) => {
           return users.includes(username)
             ? Promise.reject({
-                title: "Username in use",
-                message: "Please choose another username",
+                title: 'Username in use',
+                message: 'Please choose another username',
               })
             : rooms
                 .doc(roomCode)
-                .collection("users")
+                .collection('users')
                 .doc()
                 .set({ host: false, name: username, points: 0 });
         })
       : Promise.reject({
-          title: "Room does not exist",
-          message: "Please enter a valid room code",
+          title: 'Room does not exist',
+          message: 'Please enter a valid room code',
         });
   });
 };
 
-module.exports = { rooms, createRoom, joinRoom, getUsersInRoom };
+const getPic = (array, round) => {
+  let num = array[round - 1];
+  return firebase.storage().ref().child(`/${num}.jpg`).getDownloadURL();
+};
+
+module.exports = { rooms, createRoom, joinRoom, getUsersInRoom, getPic };
