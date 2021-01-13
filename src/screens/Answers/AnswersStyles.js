da3cc2d7bc9ac7af23a1f71130f43e91cd8d8c00
James Collins <jimwcollins@gmail.com>
2021-01-13 16:03:19 +0000

    changes loading behaviour and restyles picture shadows

--- src/screens/Answers/AnswersStyles.js
+++ src/screens/Answers/AnswersStyles.js
@@ -12,14 +12,21 @@ export default StyleSheet.create({
     top: 0,
     height: '100%',
   },
-  picContainer: {
+  picShadow: {
     width: '90%',
     height: '50%',
-    borderRadius: 5,
+    shadowColor: 'black',
+    shadowOffset: { width: 1, height: 1 },
+    shadowRadius: 5,
+    shadowOpacity: 0.5,
+    marginTop: 10,
+    marginBottom: 22,
+  },
+  picContainer: {
+    width: '100%',
+    height: '100%',
+    borderRadius: 10,
     overflow: 'hidden',
-    marginVertical: 22,
-    borderColor: 'white',
-    borderWidth: 3,
   },
   pic: {
     height: '100%',
