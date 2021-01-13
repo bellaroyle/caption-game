171ef36d40a1bfcece0d57675a216fcb6ca68b37
bella <bella.royle@gmail.com>
2021-01-13 14:19:49 +0000

    styles winners page

--- src/components/WinnersCard.js
+++ src/components/WinnersCard.js
@@ -1,19 +1,54 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default function WinnersCard({ data: { name, overallScore } }) {
   return (
     <View style={styles.container}>
-      <Text>{name}</Text>
-      <Text>{overallScore}</Text>
+      <View style={styles.subBox}>
+        <Ionicons name="trophy" size={60} color="gold" />
+      </View>
+      <View>
+        <Text style={styles.name}>{name}</Text>
+        <Text style={styles.points}>Points: {overallScore}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
-    margin: 10,
-    height: 40,
+    backgroundColor: 'white',
+    borderRadius: 10,
+    borderWidth: 3,
+    borderColor: '#820263',
+    padding: 20,
+    margin: 25,
+    shadowColor: '#2E294E',
+    shadowOffset: { width: 1, height: 1 },
+    shadowRadius: 1,
+    shadowOpacity: 0.3,
+    elevation: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    minWidth: '70%',
+  },
+  subBox: {
+    position: 'absolute',
+    top: -47,
+    right: -50,
+    backgroundColor: '#2E294E',
+    paddingHorizontal: 18,
+    borderRadius: 90,
+    padding: 15,
+    alignItems: 'center',
+    zIndex: 20,
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 50,
+  },
+  points: {
+    fontSize: 35,
   },
 });
--- src/screens/Winners/WinnersStyles.js
+++ src/screens/Winners/WinnersStyles.js
@@ -3,12 +3,22 @@ import { StyleSheet } from 'react-native';
 export default StyleSheet.create({
   screen: {
     flex: 1,
-    backgroundColor: 'dodgerblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    height: '100%',
+  },
   answerList: {
-    width: '80%',
-    marginBottom: 10,
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    marginBottom: 80,
   },
 });
